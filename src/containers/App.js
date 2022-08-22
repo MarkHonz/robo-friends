dfdbcd2722652dc5535a7090dd7f450b74eae093
markhonz <markhonz@protonmail.com>
2022-08-22 08:32:00 -0400

    Commit before adding Redux. Also added button counter.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: '',
-		};
-	}
+const App = () => {
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState('');
+	let [count, setCount] = useState(0);
 
-	componentDidMount() {
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((response) => {
 				return response.json();
 			})
 			.then((users) => {
-				this.setState({ robots: users });
+				setRobots(users);
 			});
-	}
+		console.log(count);
+	}, [count]);
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
+	useEffect(() => {
+		console.log(robots);
+	}, [robots]);
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value);
 	};
 
-	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter((robot) => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		});
-		if (!robots.length) {
-			return <h1 className="tc">Loading</h1>;
-		} else {
-			return (
-				<div className="tc">
-					<h1>Robo-Friends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
-					<Scroll>
-						<CardList robots={filteredRobots} />
-					</Scroll>
-				</div>
-			);
-		}
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	});
+	if (!robots.length) {
+		return <h1 className="tc">Loading</h1>;
+	} else {
+		return (
+			<div className="tc">
+				<h1>Robo-Friends</h1>
+				<button onClick={() => setCount(count++)}>Click Me!</button>
+				<p>{count}</p>
+				<SearchBox searchChange={onSearchChange} />
+				<Scroll>
+					<CardList robots={filteredRobots} />
+				</Scroll>
+			</div>
+		);
 	}
-}
+};
 
 export default App;
